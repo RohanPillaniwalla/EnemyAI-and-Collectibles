16f148668ad78ba1503365db2f620d75888d85a4
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-08-28 20:19:50 +0530

    updated flyer controls

--- EnemyAI and Collectibles/Assets/Scripts/FlyerController.cs
+++ EnemyAI and Collectibles/Assets/Scripts/FlyerController.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class FlyerController : MonoBehaviour
 {
+    Rigidbody2D flyerRB2D;
+ 
+    public bool vertical;
+    float timer;
+    public float flyerSpeed = 3.0f;
+    public float flyerChangeTime = 3.0f;
+    int direction = 1;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        flyerRB2D = GetComponent<Rigidbody2D>();
+
+        timer = flyerChangeTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = flyerChangeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = flyerRB2D.position;
+
+        if(vertical)
+        {
+            position.y += Time.deltaTime * flyerSpeed * direction;
+        }
+        else 
+            position.x += Time.deltaTime * flyerSpeed * direction;
+
+        flyerRB2D.MovePosition(position);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+
+        if(player != null)
+        {
+            player.ChangeHealth(-1);
+            Destroy(gameObject);
+        }
     }
 }
