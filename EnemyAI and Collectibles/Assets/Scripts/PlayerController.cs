49bcd550bcfa2e29ab6cf00e2787b1005e18edb1
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-08-31 05:15:42 +0530

    added invincibility for player for few seconds

--- EnemyAI and Collectibles/Assets/Scripts/PlayerController.cs
+++ EnemyAI and Collectibles/Assets/Scripts/PlayerController.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float playerSpeed = 3.0f;
-
+    public float timeInvincible = 2.0f;
     public int playerMaxHealth = 5;
-    int playerCurrentHealth;
-    public int Health { get { return playerCurrentHealth; } }
-
-    Rigidbody2D playerRb2D;
 
     float horizontal;
     float vertical;
+    float invincibleTimer;
+    int playerCurrentHealth;
+    bool isInvincible;
+
+    Rigidbody2D playerRb2D;
+
+    public int Health { get { return playerCurrentHealth; } }
 
     // Start is called before the first frame update
     void Start()
@@ -26,10 +29,19 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+
+        //check for invicible
+        if (isInvincible)
+        {
+            invincibleTimer -= Time.deltaTime;
+            if (invincibleTimer < 0)
+                isInvincible = false;
+        }
     }
 
     private void FixedUpdate()
     {
+        //handling movement
         Vector2 position = playerRb2D.position;
 
         position.x += playerSpeed * horizontal * Time.deltaTime;
@@ -38,8 +50,18 @@ public class PlayerController : MonoBehaviour
         playerRb2D.MovePosition(position);
     }
 
+    //method to change health on collision with health collectibles and enemy
     public void ChangeHealth(int amount)
     {
+        if (amount < 0)
+        {
+            if (isInvincible)
+                return;
+            
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
+
         playerCurrentHealth = Mathf.Clamp(playerCurrentHealth + amount, 0, playerMaxHealth);
         Debug.Log(playerCurrentHealth + "/" + playerMaxHealth);
     }
