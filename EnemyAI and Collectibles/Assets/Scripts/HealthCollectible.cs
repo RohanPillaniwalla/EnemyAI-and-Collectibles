920ce012a4cbd11605394a5a929f5b6adba2059b
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-08-31 13:29:52 +0530

    Updated health script

--- EnemyAI and Collectibles/Assets/Scripts/HealthCollectible.cs
+++ EnemyAI and Collectibles/Assets/Scripts/HealthCollectible.cs
@@ -7,10 +7,12 @@ public class HealthCollectible : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerController controller = collision.GetComponent<PlayerController>();
+
         if(controller != null)
         {
             if(controller.Health < controller.playerMaxHealth)
             {
+                Debug.Log("Player Healed");
                 controller.ChangeHealth(1);
                 Destroy(gameObject);
             }
