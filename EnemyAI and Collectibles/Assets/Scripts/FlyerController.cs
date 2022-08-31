1f9ffbd3f186f18188cd8134bb14d1c911a5bc2e
RohanPillaniwalla <58022750+RohanPillaniwalla@users.noreply.github.com>
2022-08-31 13:36:52 +0530

    reduced the direction change time

--- EnemyAI and Collectibles/Assets/Scripts/FlyerController.cs
+++ EnemyAI and Collectibles/Assets/Scripts/FlyerController.cs
@@ -7,9 +7,10 @@ public class FlyerController : MonoBehaviour
     Rigidbody2D flyerRB2D;
  
     public bool vertical;
-    float timer;
     public float flyerSpeed = 3.0f;
-    public float flyerChangeTime = 3.0f;
+    public float flyerChangeTime = 2.5f;
+
+    float timer;
     int direction = 1;
 
     // Start is called before the first frame update
@@ -22,6 +23,7 @@ public class FlyerController : MonoBehaviour
 
     void Update()
     {
+        //changing flyer direction after every 2.5 seconds
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -35,6 +37,7 @@ public class FlyerController : MonoBehaviour
     {
         Vector2 position = flyerRB2D.position;
 
+        //moving the flyer either vertically or horizotally
         if(vertical)
         {
             position.y += Time.deltaTime * flyerSpeed * direction;
@@ -45,12 +48,14 @@ public class FlyerController : MonoBehaviour
         flyerRB2D.MovePosition(position);
     }
 
+    // checking for player trigger collison with flyer
     private void OnCollisionEnter2D(Collision2D collision)
     {
         PlayerController player = collision.gameObject.GetComponent<PlayerController>();
 
         if(player != null)
         {
+            Debug.Log("Player collided with a flyer.");
             player.ChangeHealth(-1);
             Destroy(gameObject);
         }
